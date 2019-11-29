ef15006ff3602b620848c9746f57a617b7b79a44
CSharper96 <thomashorny@gmx.net>
2019-11-29 10:46:47 +0100

    empty-

---
+++ src/horny/prog1/exercises/set08/Point.java
@@ -0,0 +1,20 @@
+package horny.prog1.exercises.set08;
+
+public class Point
+{
+    private double X;
+    private double Y;
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public double GetX()
+    {
+        return X;
+    }
+    public  double GetY()
+    {
+        return Y;
+    }
+}
