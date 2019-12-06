49c58950b74be17018d94067000f206b5f8d13b5
CSharper96 <thomashorny@gmx.net>
2019-12-06 09:54:35 +0100

    Exercise08 update

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,13 +1,10 @@
+import horny.prog1.exercises.set08.*;
 public class MainClass
 {
-//    public static void main(String[] args)
-//    {
-//        ExerciseSet02 setTwo = new ExerciseSet02();
-//        int num = Terminal.readInt("Provide a number");
-//        System.out.println(setTwo.computeSum(num));
-//        setTwo.printSequence1(num);
-//        setTwo.printSequence2(num);
-//        setTwo.printSequence3(num);
-//        setTwo.printTriangle(num);
-//    }
+
+    public static void main(String[] args)
+    {
+        Prog prog = new Prog();
+        prog.startProg();
+    }
 }
---
+++ src/horny/prog1/exercises/set08/RegressionCalculator.java
@@ -0,0 +1,4 @@
+package horny.prog1.exercises.set08;
+
+public class RegressionCalculator {
+}
